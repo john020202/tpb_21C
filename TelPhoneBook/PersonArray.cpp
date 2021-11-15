7f14c5ddb7d0dd437bf2ab51a3aaa6012056b883
송승준 <john091122@naver.com>
2021-11-15 11:34:39 +0900

    4.전화번호 검색(SearchPersonArray) 삭제, 텍스트파일 출력을 위한 기능 구현

--- TelPhoneBook/PersonArray.cpp
+++ TelPhoneBook/PersonArray.cpp
@@ -1,22 +1,72 @@
-#include "PersonArray.h"
+void SearchPersonArray(PersonArray* pa)
+{
+    char name[20];
+    printf("검색 이름 입력:");
+    gets_s(name, 20);
+    for (int i = 0; i < pa->perCount; ++i)
+    {
+        if (strcmp(name, pa->arr[i].name) == 0)
+        {
+            printf("찾은 이름: [%d] :", i);
+            PrintPerson(&pa->arr[i]);
+            break;
+        }
+    }
+}
+void RemovePersonArray(PersonArray* pa)
+{
+    char name[20];
+    printf("삭제 이름 입력:");
+    gets_s(name, 20);
+    for (int i = 0; i < pa->perCount; ++i)
+    {
+        if (strcmp(name, pa->arr[i].name) == 0)
+        {
+            printf("삭제할 이름: [%d] :", i);
+            PrintPerson(&pa->arr[i]);
 
-void InitPersonArray(PersonArray* pa)
-{
-	pa->perCount = 0;
-}
-void UninitPersonArray(PersonArray* pa)
-{
-	pa->perCount = 0;
-}
-void AddPersonArray(PersonArray* pa)
-{
-	Person per;
+            for (int j = i; j < pa->perCount - 1; ++j)
+                pa->arr[j] = pa->arr[j + 1];
+            --pa->perCount;
+            printf("삭제 완료~!\n");
 
-	InputPerson(&per);
-	pa->arr[pa->perCount++] = per;
+            break;
+        }
+    }
 }
-void PrintPersonArray(PersonArray* pa)
+void WriteTextPersonArray(PersonArray* pa)
 {
-	for (int i = 0; i < pa->perCount; ++i)
-		PrintPerson(&pa->arr[i]);
+    FILE* fp = NULL;
+
+    fopen_s(&fp, "telphonebook.txt", "w");
+    if (fp == NULL)
+        return;
+
+    for (int i = 0; i < pa->perCount; ++i)
+        fprintf(fp, "Person : [%d]-name:%s, phone:%s\n", i, pa->arr[i].name, pa->arr[i].phone);
+
+    fclose(fp);
 }
+
+//#include "PersonArray.h"
+//
+//void InitPersonArray(PersonArray* pa)
+//{
+//	pa->perCount = 0;
+//}
+//void UninitPersonArray(PersonArray* pa)
+//{
+//	pa->perCount = 0;
+//}
+//void AddPersonArray(PersonArray* pa)
+//{
+//	Person per;
+//
+//	InputPerson(&per);
+//	pa->arr[pa->perCount++] = per;
+//}
+//void PrintPersonArray(PersonArray* pa)
+//{
+//	for (int i = 0; i < pa->perCount; ++i)
+//		PrintPerson(&pa->arr[i]);
+//}
